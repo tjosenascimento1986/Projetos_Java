8a5b3572bc242548f7b9807556c0e2fd54a2e4f5
thiago jose <tjosenascimento@gmail.com>
2026-03-26 11:25:51 -0300

    atualizando v2Final

--- produtosapi/src/main/java/com/tjn/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/com/tjn/produtosapi/controller/ProdutoController.java
@@ -2,11 +2,10 @@ package com.tjn.produtosapi.controller;
 
 import com.tjn.produtosapi.model.Produto;
 import com.tjn.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 //teste
@@ -28,5 +27,26 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPorId(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable("id")String id,@RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+       return produtoRepository.findByNome(nome);
     }
 }
--- produtosapi/src/main/java/com/tjn/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/com/tjn/produtosapi/repository/ProdutoRepository.java
@@ -3,6 +3,9 @@ package com.tjn.produtosapi.repository;
 import com.tjn.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto,String > {
 
+    List<Produto> findByNome(String nome);
 }
