5db15f13e7701355d2f9a4f8cfd893106617e3a1
thiago jose <tjosenascimento@gmail.com>
2026-04-01 16:22:27 -0300

    ver1.0.1

--- parkcontrol/src/main/java/com/tjn/parkcontrol/controller/RegistroController.java
+++ parkcontrol/src/main/java/com/tjn/parkcontrol/controller/RegistroController.java
@@ -1,7 +1,22 @@
 package com.tjn.parkcontrol.controller;
 
+import com.tjn.parkcontrol.repository.RegistroRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("carro")
 public class RegistroController {
-    public void cadastrarEntrada(){
+
+    private RegistroRepository registroRepository;
+    public RegistroController(RegistroRepository registroRepository) {
+        this.registroRepository = registroRepository;
+    }
+
+    @PostMapping
+    public void cadastrarEntrada(@RequestBody String placa){
 
     }
     public void finalizarEstadia(){
